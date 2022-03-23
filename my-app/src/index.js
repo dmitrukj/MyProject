3266b845a881314464d73c781009c92cc954b601
dmitruk <dmitruk_89@mail.ru>
2022-03-23 15:16:21 +0300

    moved the repositories to a separate file

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./Redux/state";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let rerenderEntireTree = (state) => {
+  debugger;
+  ReactDOM.render(
+    <BrowserRouter>
+      <App
+        state={state.store}
+        repos={store.repos}
+        repos2={store.repos2}
+        repos3={store.repos3}
+        repos4={store.repos4}
+        repos5={store.repos5}
+      />
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
+};
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
